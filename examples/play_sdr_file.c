88da3b1a618a34cca00cf08a5316ae66ccd85e3a
Daniel Hjort <daniel.hjort@gmail.com>
2014-03-02 19:47:54 +0900

    Adding IQ test file with SSB traffic and use that in the play_sdr_file example.

--- examples/play_sdr_file.c
+++ examples/play_sdr_file.c
@@ -16,7 +16,7 @@
 #define AUDIO_FRAME_SIZE 2048
 #define IQ_FRAME_SIZE 2 * AUDIO_FRAME_SIZE
 #define SAMPLE_RATE 48000
-#define BFO_FREQ 1000
+#define BFO_FREQ 5400
 
 int main() {
 
@@ -32,7 +32,7 @@ int main() {
 	Demodulator demod;
 
 	reader_options.type = FILE_TYPE;
-	reader_options.filename = "../testdata/iq_cf7020kHz_sf48kHz_sint16.raw";
+	reader_options.filename = "../testdata/iq_cf14181kHz_sf48kHz_sint16.raw";
 
 	player_options.encoding = SINT16;
 	player_options.sample_rate = SAMPLE_RATE;
@@ -46,7 +46,7 @@ int main() {
 
 	nami_init_gain(&gain);
 	nami_osc_init(&bfo, nami_angular_frequency_from_frequency(BFO_FREQ), SAMPLE_RATE);
-	nami_demod_init(&demod, &bfo, &nami_demod_lsb);
+	nami_demod_init(&demod, &bfo, &nami_demod_usb);
 
         nami_open_reader(&reader, reader_options);
 	nami_open_player(&player, player_options);
@@ -54,7 +54,7 @@ int main() {
 	// Loop until done
 	while (nami_read_frame(&reader, &iq_frame) > 0) {
 
-		nami_apply_gain(&gain, &iq_frame, 20, &gainResult);
+		nami_apply_gain(&gain, &iq_frame, 30, &gainResult);
 		nami_demod_frame(&demod, &iq_frame, &audio_frame);
 		nami_play_frame(&player, &audio_frame);
 	}
